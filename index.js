ef63ed42f4841d670cb9da41b226b7dc004273f5
Jenny Chau <jnechau@gmail.com>
2021-09-10 14:13:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    "harry" : "cupboard under the stairs",
+    "ron" : "the burrow", 
+    "hermione" : "the library",
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, address){
+    const newEmployee = {...employee};
+    newEmployee[name] = address;
+    return newEmployee;
+}
+
+console.log(employee)
+console.log (updateEmployeeWithKeyAndValue(employee, "mcgonagall", "Hogwarts"));
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, address){
+    employee[name] = address;
+    return employee;
+}
+
+console.log (destructivelyUpdateEmployeeWithKeyAndValue(employee, "sirius", "Grimmauld Place 12"));
+
+function deleteFromEmployeeByKey(employee, name){
+    let lessEmployee = {...employee};
+    delete lessEmployee[name];
+    return lessEmployee;
+}
+
+console.log(deleteFromEmployeeByKey(employee, "ron"));
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+    return employee;
+}
+
+console.log(employee)
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "harry"));
